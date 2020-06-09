538e096104b5d4bbae6dc842a0c592ac47bdbfaf
Shamila Corless <shamilacorless@iMac.local>
2020-06-09 12:19:46 -0400

    initial app commit

---
+++ loginid-sdk-sample/loginid-sdk-sampleTests/loginid_sdk_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  loginid_sdk_sampleTests.swift
+//  loginid-sdk-sampleTests
+//
+//  Created by Shamila Corless on 2020-06-09.
+//  Copyright © 2020 hatchways. All rights reserved.
+//
+
+import XCTest
+@testable import loginid_sdk_sample
+
+class loginid_sdk_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
