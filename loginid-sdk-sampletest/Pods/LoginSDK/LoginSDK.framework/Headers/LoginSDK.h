da5beca0996e0a27c3cb82b1028ec18d7e0bf416
Shamila Corless <shamilacorless@iMac.local>
2020-06-11 12:23:23 -0400

    Renamed folder to specify that this is the test app

---
+++ loginid-sdk-sampletest/Pods/LoginSDK/LoginSDK.framework/Headers/LoginSDK.h
@@ -0,0 +1,19 @@
+//
+//  LoginidSDK.h
+//  LoginidSDK
+//
+//  Created by PD on 2019-11-07.
+//  Copyright © 2019 Loginid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoginidSDK.
+FOUNDATION_EXPORT double LoginSDKVersionNumber;
+
+//! Project version string for LoginidSDK.
+FOUNDATION_EXPORT const unsigned char LoginSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginidSDK/PublicHeader.h>
+
+
