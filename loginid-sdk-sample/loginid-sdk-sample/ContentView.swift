2a2e57fd7eaa1eadce39a8b49a1fcccfbf510cc9
Shamila Corless <shamilacorless@iMac.local>
2020-06-11 15:42:19 -0400

    Created sample app project

---
+++ loginid-sdk-sample/loginid-sdk-sample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  loginid-sdk-sample
+//
+//  Created by Shamila Corless on 2020-06-11.
+//  Copyright © 2020 hatchways. All rights reserved.
+//
+
+import SwiftUI
+import LoginSDK
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
